4a30f79346fb4c9ef684549f0c1c7618beb70308
suginogaku <g-sugino@macro-eg.co.jp>
2025-07-14 16:19:14 +0900

    タスク取得API実装

---
+++ src/app/api/tasks/[id]/route.ts
@@ -0,0 +1,23 @@
+import { TaskModel } from "@/models/task";
+import { connectToDatabase } from "@/utils/database";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (_: NextRequest, { params }: {params: {id: string}}) => {
+  try {
+    await connectToDatabase()
+    const task = await TaskModel.findById(params.id)
+    if (!task) {
+      return NextResponse.json(
+        {message: "タスクが存在しません"},
+        {status: 404})
+    }
+
+    return NextResponse.json({ message: "タスク取得成功", task })
+
+  } catch(error) {
+    console.log(error);
+    return NextResponse.json({ message: "タスク取得失敗" }, { status: 500 })
+  }
+}
+
+export const dynamic = 'force-dynamic'
